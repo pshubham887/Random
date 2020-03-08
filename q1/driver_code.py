f4fe91cb9dec562fce1a606414d52bd58d7df67f
pshubham887 <pshubham887@gmail.com>
2020-03-08 14:49:13 +0530

    Add files via upload

---
+++ q1/driver_code.py
@@ -0,0 +1,10 @@
+import json
+
+def auto_complete_results(prefix):
+    # Complete code here
+    pass
+
+with open('C:\\tools\\goland2\\goland2\\config\\scratches\\auto_fill_data.json') as json_file:
+    jsonData = json.load(json_file)
+    
+testcase_count = int(input())
